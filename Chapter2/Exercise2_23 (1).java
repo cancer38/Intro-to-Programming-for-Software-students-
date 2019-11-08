73db3ecd9687d68c7cc463e13ad0fece12a37796
cancer38 <56974251+cancer38@users.noreply.github.com>
2019-11-08 13:48:03 -0700

    Add files via upload

---
+++ Chapter2/Exercise2_23 (1).java
@@ -0,0 +1,26 @@
+// Skylynn Nicholls
+// CSCI 1105
+// Exercise 2_23
+
+import java.util.Scanner;
+public class Exercise2_23 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+				// Prompt the user to enter the distace to drive, the fuel 
+				// efficiency of the car in miles and the price per gallon.
+				System.out.print("Enter the driving distace: ");
+				double distace = input.nextDouble();
+				System.out.print("Enter miles per gallon: ");
+				double milesPerGallon = input.nextDouble();
+				System.out.print("Enter price per gallon: ");
+				double pricePerGallon = input.nextDouble();
+
+				// Compute the cost of driving
+				double costOfDriving = (distace / milesPerGallon) * pricePerGallon;
+
+				// Display result
+				System.out.println("The cost of driving is $" + costOfDriving);
+
+	}
+}
