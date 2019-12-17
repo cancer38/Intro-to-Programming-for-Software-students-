335c259bc34675ea9b42674202843391abe78ed7
cancer38 <56974251+cancer38@users.noreply.github.com>
2019-12-17 14:08:56 -0700

    Add files via upload

--- chapter 5/Exercise5_11.java
+++ chapter 5/Exercise5_11.java
@@ -1,17 +1,17 @@
- class Exercise5_11 {
+class Exercise5_11 {
 	public static void main(String[] args) {
-		final int NUMBERS_PER_LINE = 10;	
-		int count = 0; 
-
-		for (int j = 100; j <= 200; j++) {
-			if (j % 5 == 0 ^ j % 6 == 0) {
+		final int NUMBER_PER_LINE = 10;
+		int count = 0;
+		
+		for(int k = 100; k <= 200; k++){
+			if(k % 5 == 0 ^ k % 6 == 0){
 				count++;
-				if (count % NUMBERS_PER_LINE == 0)
-					System.out.println(j);
-				else
-					System.out.print(j + " ");			
+				System.out.print(k + " ");
 			}
-		}
-		System.out.println();
+			if(NUMBER_PER_LINE == count){
+				System.out.println();
+				count = 0;
+			}
+		}	
 	}
 }
